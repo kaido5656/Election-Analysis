7c277b583310af3198418cc68ee9929ffaf93d75
kaido5656 <78507488+kaido5656@users.noreply.github.com>
2021-03-18 14:35:56 -0500

    adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -7,20 +7,81 @@
 
 import csv
 import os
-#assign a variable for the file to load and the path
+#assign a variable for the load a file from a path
 file_to_load = os.path.join("Resources", "election_results.csv")
-
-#create a filename valriable to a direct or indirect path to the the file.
+#assign a variable to save the file to a path
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+#1initialize a total vote counter
+total_votes = 0
+
+#canidate options and votes
+canidate_options = []
+canidate_votes = {}
+
+#winning canidate and winning count tracker
+winning_canidate = ""
+winning_count = 0
+winning_percentage = 0
 
 #open the election results and read the file
 with open(file_to_load) as election_data:
     #read th file object with the reader function
     file_reader = csv.reader(election_data)
-    #print the header row
+    
+    #read the header row
     headers = next(file_reader)
-    print(headers)
-#close the file
+    
+    #print each row in csv file
+    for row in file_reader:
+        #2 add to the total vote count
+        total_votes += 1
+
+
+        canidate_name = row[2]
+
+        #if the canidate does not match any existing canidate..
+        if canidate_name not in canidate_options:
+        #add it to the list of canidates               
+             canidate_options.append(canidate_name)
+
+         #begin tracking each canidates vote count
+             canidate_votes[canidate_name] = 0
+        
+        #add a vote to that canidate's coount
+        canidate_votes[canidate_name] += 1
+    #determine the percentage of votes for each canidate by looping through the counts
+    #iterate through the canidate list
+    for canidate_name in canidate_votes:
+
+        #retreive vote count of a canidate
+        votes = canidate_votes[canidate_name]
+        #calculate the percent of votes
+        vote_percentage = float(votes) / float(total_votes) *100
+        #print the canidate name and percentage of votes
+        
+        print(f"{canidate_name}: {vote_percentage:.1f}%  ({votes:,})\n")
+
+        #determine winning vote count
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # if true then set winningcount = votes and winningpercent = vote percent
+            winning_count = votes
+            winning_percentage = vote_percentage
+            #and set the winning canidate equal to the canidates name
+            winning_canidate = canidate_name
+
+    winning_canidate_summary = (
+            f"------------------------\n"
+            f"Winner: {winning_canidate}\n"
+            f"Winning Vote Count: {winning_count:,}\n"
+            f"winning Percentage: {winning_percentage:.1f}%\n"
+            f"------------------------\n" )
+    print(winning_canidate_summary)
+#print total votes and canidate list
+
+#print(canidate_votes)
+#print(canidate_options)
+#print(total_votes)
 election_data.close()
 
 #using the with statement open the file as a text file
